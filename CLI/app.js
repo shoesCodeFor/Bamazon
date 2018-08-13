f454891112ba2c25edc24e0868c9bbe64615c2e9
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-08-13 03:04:22 -0600

    CLI is working, but needs refinement in the prompts.

--- CLI/app.js
+++ CLI/app.js
@@ -1,6 +1,26 @@
 require("dotenv").config();
 const mysql = require("mysql");
-console.log(process.env.TWITTER_CONSUMER_KEY);
+const inquirer = require("inquirer");
+var productList;
+var productNames = [];
+
+
+console.log("CLI Loaded")
+console.log(process.env.MYSQL_HOSTNAME);
+console.log(process.env.MYSQL_USERNAME);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_PORT);
+console.log(process.env.MYSQL_DB);
+
+const config = {
+    host: process.env.MYSQL_HOSTNAME,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DB
+};
+
+// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -15,19 +35,94 @@ const connection = mysql.createConnection({
     password: "",
     database: "bamazon"
 });
+const startCLI = () => {
+    inquirer
+        .prompt([
+            /* Bamazon Questions */
+            // The first should ask them the ID of the product they would like to buy.
+            {
+                name: 'productName',
+                type: 'list',
+                choices: productNames,
+                message: "What Product Would You like to Buy?"
+            },
+            {
+                name: 'quantity',
+                type: 'input',
+                message: "How Many Would You like to Buy?"
+            }
+            // The second message should ask how many units of the product they would like to buy.
+        ])
+        .then(answers => {
+            // Use user feedback for transaction!!
+            let productName = answers.productName;
+            let quantity = answers.quantity;
+            const result = productList.find( product => product.product_name === productName );
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-
-});
-
-const allProducts = function (){
-    connection.query('SELECT * from products', function(err, rows, fields) {
-        if(err) console.log(err);
-        console.log('The solution is: ', rows);
-        connection.end();
-    });
+            console.log(`Selected Prod:`);
+            console.log(result);
+            console.log(answers);
+            console.log("Let us check if we have this in stock....");
+            updateProductQty(result, quantity);
+        });
 };
 
+
+
+connection.connect(function(err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId + "\n");
+
+    });
+
+
+
+const allProducts = () => {
+
+    connection.query('SELECT * from products', function(err, rows, fields) {
+        if(err) console.log(err);
+        // console.log('The solution is: ', rows);
+        productList = rows;
+        rows.forEach(row => {
+            productNames.push(row.product_name);
+        });
+        startCLI();
+    });
+};
+const getProduct = (prodID, cb = undefined) => {
+    let qty;
+    connection.query(`SELECT * from products where product_id = '${prodID}'`, function(err, rows, fields) {
+        if(err) console.log(err);
+        console.log('All products: ', rows);
+
+        qty = rows[0].stock_quantity-1;
+        console.log('Inventory Updated: ' + qty);
+        if(cb){
+            return cb(prodID, qty)
+        }
+        connection.end();
+
+    });
+
+};
+
+const updateProductQty = (prod, qty) => {
+    let prodID = prod.product_id;
+    let newQty = prod.stock_quantity - qty;
+    connection.query(`UPDATE \`products\` SET \`stock_quantity\` = '${newQty}' WHERE \`products\`.\`product_id\` = ${prodID};`, function(err, rows, fields) {
+        if(err) console.log(err);
+        console.log('Single Product: ', rows);
+        // connection.end();
+    });
+    getProduct(prodID);
+    // allProducts();
+};
+
+
+
 allProducts();
+//
+// // updateProductQty(7, 500);
+//
+// getProduct(7, updateProductQty);
+// allProducts();
