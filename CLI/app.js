7cd7f618d6b8dfd8f8ad48fd07ff1fcd7c4d0e1e
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-08-12 23:11:26 -0600

    We have a serious mess on our hands.  Trying to fix the CLI tool.

---
+++ CLI/app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const mysql = require("mysql");
+var express = require('express');
+var router = express.Router();
+
+
+console.log(process.env.TWITTER_CONSUMER_KEY);
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+});
+
+const allProducts = function (){
+    connection.query('SELECT * from products', function(err, rows, fields) {
+        if(err) console.log(err);
+        console.log('The solution is: ', rows);
+        connection.end();
+    });
+};
+
+allProducts();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bamazon
-An online ordering clone
+An online ordering clone of that other service.
+
+This is a faux service with no real purchasing ability.  You can, however, select items, 'check out' and watch the changes in the DB.
+
+## Using the CLI tool
+
